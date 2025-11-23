9d4554ff30bc06a337c85aaa6dde0fee1f0877d5
Hamza Mughales <hamzawemughales@gmail.com>
2025-11-23 21:26:18 +0300

    feat: migrations files

---
+++ database/migrations/2025_11_16_190041_create_reminder_logs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reminder_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('subscriber_id')
+                ->constrained('subscribers')
+                ->cascadeOnDelete();
+            $table->text('message');
+            $table->string('status'); // "sent" | "failed"
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reminder_logs');
+    }
+};
