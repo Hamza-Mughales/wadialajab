f51ef1f2809fb537536230c846564cc8600e90ee
Hamza Mughales <hamzawemughales@gmail.com>
2025-11-24 20:23:56 +0300

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Jobs\SendMonthlyRemindersJob;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::job(new SendMonthlyRemindersJob())
+    ->monthlyOn(28, '09:00');
