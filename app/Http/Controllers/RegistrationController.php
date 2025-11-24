4a35f991f078389c1cd68c33d6c5f39718426c26
Hamza Mughales <hamzawemughales@gmail.com>
2025-11-24 20:09:43 +0300

    feat: register controller

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSubscriberRequest;
+use App\Models\Subscriber;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+
+class RegistrationController extends Controller
+{
+    public function create(): View
+    {
+        return view('join');
+    }
+
+    public function store(StoreSubscriberRequest $request): RedirectResponse
+    {
+        Subscriber::create($request->validated());
+
+        return redirect()->route('join.success');
+    }
+
+    public function success(): View
+    {
+        return view('join-success');
+    }
+}
+
+
---
+++ app/Http/Requests/StoreSubscriberRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreSubscriberRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'full_name' => ['required', 'string', 'max:255'],
+            'phone'     => ['required', 'string', 'max:50'],
+            'amount'    => ['required', 'numeric', 'min:0'],
+            'email'     => ['nullable', 'email', 'max:255'],
+        ];
+    }
+}
+
+
