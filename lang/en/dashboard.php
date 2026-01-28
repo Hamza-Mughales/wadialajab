bc9c1900d66a0d01b92e341e7c4cf604657758b2
Hamza Mughales <hamzawemughales@gmail.com>
2026-01-28 21:04:02 +0300

    feat: add dashboard translation files

---
+++ lang/en/dashboard.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'stats' => [
+        'applications' => [
+            'label' => 'Applications',
+            'description' => 'Total applications received',
+        ],
+        'pending_applications' => [
+            'label' => 'Pending Applications',
+            'description' => 'Needs review',
+        ],
+        'subscribers' => [
+            'label' => 'Subscribers',
+            'description' => 'Total active subscribers',
+        ],
+    ],
+    'latest_applications' => [
+        'heading' => 'Latest Applications',
+        'columns' => [
+            'date' => 'Date',
+            'name' => 'Name',
+            'status' => 'Status',
+        ],
+        'actions' => [
+            'view' => 'View',
+        ],
+    ],
+    'applications_chart' => [
+        'heading' => 'Applications Over Time',
+        'label' => 'Applications',
+    ],
+];
